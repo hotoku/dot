3660411464a8e978d904d73f87238ffef9a44a14
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-04-18 11:11:26 +0900

    conda completion

---
+++ bash_profile.sh
@@ -0,0 +1,55 @@
+export PS1="\[\e[0;32m\]\W \$ \[\e[00m\]"
+# export PROMPT_COMMAND="history -a; history -r; $PROMPT_COMMAND"
+export HISTSIZE=50000
+export EDITOR=emacsclient
+
+
+eval "$(direnv hook bash)"
+
+
+alias gst="git status"
+alias gc="git commit"
+alias tiga="tig --all"
+
+
+_conda(){
+    local commands="activate
+deactivate
+clean
+config
+create
+help
+info
+init
+install
+list
+package
+remove
+uninstall
+run
+search
+update
+upgrade
+build
+convert
+debug
+develop
+env
+index
+inspect
+metapackage
+render
+server
+skeleton
+verify"
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local prev=${COMP_WORDS[COMP_CWORD-1]}
+    if [ ${prev} = conda ]; then
+        local cand=${commands[@]}
+    elif [ ${prev} = activate ]; then
+        local cand=$(conda env list | grep -v "#" | awk '{print $1}')
+    fi
+    COMPREPLY=($(compgen -W "${cand[@]}" -- "${cur}"))    
+}
+
+complete -F _conda conda
