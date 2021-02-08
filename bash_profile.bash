26d58d8f451d592bca9ec83c9d1f91014cb94e8d
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-02-08 12:16:22 +0900

    fix

--- bash_profile.bash
+++ bash_profile.bash
@@ -1,3 +1,10 @@
+__hotoku_source_if_exists(){
+    path=$1
+    [ -f ${path} ] && source ${path}
+}
+
+
+
 # conda
 _conda(){
     local commands="activate
@@ -70,10 +77,14 @@ fi
 
 
 # fzf
-if [[ -d ~/projects/dot/fzf-extras/.git ]] || [[ -f ~/projects/dot/fzf-extras/.git ]]; then
-    source /usr/share/doc/fzf/examples/key-bindings.bash
-    source ~/projects/dot/fzf-extras/fzf-extras.sh
-fi
+__hotoku_source_if_exists ~/.fzf.bash
+__hotoku_source_if_exists ~/projects/dot/fzf-extras/fzf-extras.sh
+
+
+
+# gcloud
+__hotoku_source_if_exists /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
+__hotoku_source_if_exists /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
 
 
 # gh
@@ -86,3 +97,6 @@ fi
 [[ -d ~/.rbenv  ]] && \
     export PATH=${HOME}/.rbenv/bin:${PATH} && \
     eval "$(rbenv init -)"
+
+
+unset -f __hotoku_source_if_exists
